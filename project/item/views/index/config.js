381d785b8b5957609cdd4082d735935c51ce44e8
haohonglong <947923503@qq.com>
2016-03-12 19:01:04 +0800

    add

---
+++ project/item/views/index/config.js
@@ -0,0 +1,8 @@
+var _ROOT_ = '../../../../..';
+(function(){
+    var tag = "script",attrs=[],src;
+    attrs.push('type="text/javascript"');
+    src=_ROOT_+'/config.js';
+
+    document.write('<',tag,' ',attrs.join(' '),'src=','"',src,'"','>','<','/',tag,'>');
+})();
