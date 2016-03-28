c69dda5a8f834b9b70963e41d14f323714644d73
haohonglong <947923503@qq.com>
2016-03-28 23:01:06 +0800

    fix

--- project/controllers/index/config.js
+++
@@ -1,8 +0,0 @@
-var _ROOT_ = '../../..';
-(function(){
-    var tag = "script",attrs=[],src;
-    attrs.push('type="text/javascript"');
-    src=_ROOT_+'/config.js';
-
-    document.write('<',tag,' ',attrs.join(' '),'src=','"',src,'"','>','<','/',tag,'>');
-})();
