80ef619dbd8e96df37a82993e38edd8d390ccf86
龙昊宏 <947923503@qq.com>
2016-04-15 10:14:11 +0800

    fix bootstrap 下拉框扩充方法

--- lamborghiniJS/Tools.class.js
+++ lamborghiniJS/Tools.class.js
@@ -151,22 +151,32 @@ window[LHH_NAMESPACE_20150715_].main([window,jQuery],function(window,$,undefined
 		 * @author: lhh
 		 * 产品介绍：
 		 * 创建日期：2016-4-14
-		 * 修改日期：2016-4-14
+		 * 修改日期：2016-4-15
 		 * 名称： dropdownMenu
 		 * 功能：bootstrap下拉框选中所选的放入输入框
-		 * 说明：data-select="input" 这个属性放到 显示内容的元素
+		 * 说明：data-select="input" 这个属性放到 显示内容的元素。callBack:this (当前点击的li元素) input(当前输入框)
 		 * 注意：
-		 * @param   (String)box            NULL :输入框选择器字符
+		 * @param   (String)box            		  NULL :输入框选择器字符
+		 * @param   (Function)callBack            NULL :回调：
 		 * @return (void)
 		 * Example：
 		 *
 		 */
-		'dropdownMenu':function(box){
+		'dropdownMenu':function(box,callBack){
+			if(System.isFunction(box)) {
+				callBack = box;
+				box = undefined;
+			}
 			box = box || '.btn[data-select="input"]';
 			$(box).each(function(){
 				var input=this;
 				$(this).parent().on('click','.dropdown-menu li',function(){
 					$(input).text($(this).text());
+					if(System.isFunction(callBack)){
+						//this : li
+						//input : 当前输入框
+						callBack.call(this,input);
+					}
 				});
 			});
 
