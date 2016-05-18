96284435c3da9667aa3e45b401f7d891afa489f6
龙昊宏 <947923503@qq.com>
2016-05-18 11:24:28 +0800

    fix

--- project/controllers/config.js
+++ project/controllers/config.js
@@ -5,7 +5,7 @@ if(!_ROOT_){
 (function(){
     var tag = "script",attrs=[],src;
     attrs.push('type="text/javascript"');
-    src=_ROOT_+'/config.js';
+    src=_ROOT_+'/common/config/config.js';
 
     document.write('<',tag,' ',attrs.join(' '),'src=','"',src,'"','>','<','/',tag,'>');
 })();
