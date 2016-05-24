632dfc61f75c0d256fab5b6b479c8d308dc36a96
龙昊宏 <947923503@qq.com>
2016-05-24 14:55:59 +0800

    fix

--- lamborghiniJS/Helper.class.js
+++ lamborghiniJS/Helper.class.js
@@ -19,178 +19,172 @@ window[GRN_LHH].main(function(undefined){
 	System.is(System,'Component','Helper');
 	var __this__=null;
 
+	System.merge(null,[{
+						/**
+						 *
+						 * @author: lhh
+						 * 产品介绍：
+						 * 创建日期：2016-04-18
+						 * 修改日期：2016-04-18
+						 * 名称：getRootPath
+						 * 功能：获取项目根路径，如： http://localhost:8083/uimcardprj
+						 * 说明：
+						 * 注意：
+						 * @param (void)
+						 * @returns {string}
+						 */
+						'getRootPath':function(){
+							//获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
+							var curWwwPath=window.document.location.href;
+							//获取主机地址之后的目录，如： uimcardprj/share/meun.jsp
+							var pathName=window.document.location.pathname;
+							var pos=curWwwPath.indexOf(pathName);
+							//获取主机地址，如： http://localhost:8083
+							var localhostPaht=curWwwPath.substring(0,pos);
+							//获取带"/"的项目名，如：/uimcardprj
+							var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);
+							return(localhostPaht+projectName);
+						},
+						/**
+						 *
+						 * @author: lhh
+						 * 产品介绍：
+						 * 创建日期：2016-03-8
+						 * 修改日期：2016-03-9
+						 * 名称：analysisVar
+						 * 功能：解析变量
+						 * 说明：
+						 * 注意：
+						 * @param vars
+						 * @returns {var}
+						 */
+						'analysisVar':function(vars,
+											   v,
+											   root){
 
-	/**
-	 *
-	 * @author: lhh
-	 * 产品介绍：
-	 * 创建日期：2016-04-18
-	 * 修改日期：2016-04-18
-	 * 名称：MySystem.getRootPath
-	 * 功能：获取项目根路径，如： http://localhost:8083/uimcardprj
-	 * 说明：
-	 * 注意：
-	 * @param (void)
-	 * @returns {string}
-	 */
-	System.getRootPath=function(){
-		//获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp
-		var curWwwPath=window.document.location.href;
-		//获取主机地址之后的目录，如： uimcardprj/share/meun.jsp
-		var pathName=window.document.location.pathname;
-		var pos=curWwwPath.indexOf(pathName);
-		//获取主机地址，如： http://localhost:8083
-		var localhostPaht=curWwwPath.substring(0,pos);
-		//获取带"/"的项目名，如：/uimcardprj
-		var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);
-		return(localhostPaht+projectName);
-	};
+							if(-1 === vars.indexOf('.')){
+								return eval(vars);
+							}
 
+							v=vars.split('.');
+							root=eval(v[0]);
+							v.each(function(i){
+								if(i!=0){
+									root=root[this];
+								}
+							});
 
-	/**
-	 *
-	 * @author: lhh
-	 * 产品介绍：
-	 * 创建日期：2016-03-8
-	 * 修改日期：2016-03-9
-	 * 名称：MySystem.analysisVar
-	 * 功能：解析变量
-	 * 说明：
-	 * 注意：
-	 * @param vars
-	 * @returns {var}
-	 */
-	System.analysisVar=function(vars,
-						v,
-						root){
-
-		if(-1 === vars.indexOf('.')){
-			return eval(vars);
-		}
-
-		v=vars.split('.');
-		root=eval(v[0]);
-		v.each(function(i){
-			if(i!=0){
-				root=root[this];
-			}
-		});
-
-		return root;
+							return root;
 
 
-	};
-	/**
-	 *
-	 * @author: lhh
-	 * 产品介绍：
-	 * 创建日期：2016-03-8
-	 * 修改日期：2016-03-9
-	 * 名称：MySystem.templat
-	 * 功能：替换模版中的变量
-	 * 说明：变量式：__root__ ; 对象式：System.__root__
-	 * 注意：
-	 * @param (String)S NO NULL:要匹配的变量
-	 * @returns {String}
-	 */
-	System.template=function(S){
-		//没找到模版分隔符就返回传入的字符串
-		if(-1 === S.indexOf(System.Config.templat.leftLimit)){
-			return S ||'';
-		}
-		var ss=S.split('/'),arr=[],v=[],v2=[],$1,$2,$3,
-			L=System.Config.templat.leftLimit,
-			R=System.Config.templat.rightLimit;
+						},
+						/**
+						 *
+						 * @author: lhh
+						 * 产品介绍：
+						 * 创建日期：2016-03-8
+						 * 修改日期：2016-03-9
+						 * 名称：templat
+						 * 功能：替换模版中的变量
+						 * 说明：变量式：__root__ ; 对象式：System.__root__
+						 * 注意：
+						 * @param (String)S NO NULL:要匹配的变量
+						 * @returns {String}
+						 */
+						'template':function(S){
+							//没找到模版分隔符就返回传入的字符串
+							if(-1 === S.indexOf(System.Config.templat.leftLimit)){
+								return S ||'';
+							}
+							var ss=S.split('/'),arr=[],v=[],v2=[],$1,$2,$3,
+								L=System.Config.templat.leftLimit,
+								R=System.Config.templat.rightLimit;
 
-		ss.each(function(){
-			if(-1 === this.indexOf(L)){
-				arr.push(this);
-			}else{//如果每个里有模版标签
-				v=this.split(L);
-				$1=v[0] ? v[0] : '';
-				v2=v[1].split(L)[0].trim().split(R);
-				$2=v2[0];
-				$3=v2[1] ? v2[1] :'';
-				arr.push([$1,System.analysisVar($2),$3].join('').trim());
+							ss.each(function(){
+								if(-1 === this.indexOf(L)){
+									arr.push(this);
+								}else{//如果每个里有模版标签
+									v=this.split(L);
+									$1=v[0] ? v[0] : '';
+									v2=v[1].split(L)[0].trim().split(R);
+									$2=v2[0];
+									$3=v2[1] ? v2[1] :'';
+									arr.push([$1,System.analysisVar($2),$3].join('').trim());
 
-			}
-		});
+								}
+							});
 
-		return arr.join('/');
-	};
+							return arr.join('/');
+						},
+						/**
+						 *
+						 * @author: lhh
+						 * 产品介绍：
+						 * 创建日期：2016-03-9
+						 * 修改日期：2016-03-9
+						 * 名称：findTpl
+						 * 功能：查找模版标签
+						 * 说明：
+						 * 注意：
+						 * @param (String)S NO NULL:要查找的字符串
+						 * @returns {Array}
+						 */
+						'findTpl':function(S){
+							var ss=[],arr=[],v=[],$1,$2,
+								L=System.Config.templat.leftLimit,
+								R=System.Config.templat.rightLimit;
+							//没找到模版分隔符就返回传入的字符串
+							if(S.indexOf(L) !== -1){
+								ss=S.split(L);
+								ss.each(function(){
+									if(-1 === this.indexOf(R)){
+										arr.push(this);
+									}else{//如果每个里有模版标签
+										v=this.split(R);
+										$1=v[0];
+										$2=v[1].trim();
+										arr.push([System.analysisVar($1),System.findTpl($2)].join('').trim());
 
+									}
 
-	/**
-	 *
-	 * @author: lhh
-	 * 产品介绍：
-	 * 创建日期：2016-03-9
-	 * 修改日期：2016-03-9
-	 * 名称：MySystem.findTpl
-	 * 功能：查找模版标签
-	 * 说明：
-	 * 注意：
-	 * @param (String)S NO NULL:要查找的字符串
-	 * @returns {Array}
-	 */
-	System.findTpl=function(S){
-		var ss=[],arr=[],v=[],$1,$2,
-			L=System.Config.templat.leftLimit,
-			R=System.Config.templat.rightLimit;
-		//没找到模版分隔符就返回传入的字符串
-		if(S.indexOf(L) !== -1){
-			ss=S.split(L);
-			ss.each(function(){
-				if(-1 === this.indexOf(R)){
-					arr.push(this);
-				}else{//如果每个里有模版标签
-					v=this.split(R);
-					$1=v[0];
-					$2=v[1].trim();
-					arr.push([System.analysisVar($1),System.findTpl($2)].join('').trim());
+								});
+							}else{
+								return S ||'';
+							}
 
-				}
+							return arr;
 
-			});
-		}else{
-			return S ||'';
-		}
+						},
+						/**
+						 *
+						 * @author: lhh
+						 * 产品介绍：
+						 * 创建日期：2016-03-9
+						 * 修改日期：2016-03-9
+						 * 名称：replaceTpl
+						 * 功能：替换模版标签
+						 * 说明：
+						 * 注意：
+						 * @param (String)selector 		NO NULL:选择器
+						 * @param (String)attr_name 	NO NULL:标签属性
+						 * @param (Function)callback 	   NULL:回调函数
+						 * @returns {String}
+						 */
+						'replaceTpl':function(selector,attr_name,callback){
+							var value;
+							$(selector).each(function(){
+								if(callback && System.isFunction(callback)){
+									callback.call(this);
+								}else{
+									value=System.template($(this).attr(attr_name));
+									$(this).attr(attr_name,value);
+									console.log(value);
+								}
 
-		return arr;
-
-	};
-
-	/**
-	 *
-	 * @author: lhh
-	 * 产品介绍：
-	 * 创建日期：2016-03-9
-	 * 修改日期：2016-03-9
-	 * 名称：MySystem.replaceTpl
-	 * 功能：替换模版标签
-	 * 说明：
-	 * 注意：
-	 * @param (String)selector 		NO NULL:选择器
-	 * @param (String)attr_name 	NO NULL:标签属性
-	 * @param (Function)callback 	   NULL:回调函数
-	 * @returns {String}
-	 */
-	System.replaceTpl=function(selector,attr_name,callback){
-		var value;
-		$(selector).each(function(){
-			if(callback && System.isFunction(callback)){
-				callback.call(this);
-			}else{
-				value=System.template($(this).attr(attr_name));
-				$(this).attr(attr_name,value);
-				console.log(value);
-			}
-
-		});
-		return System;
-	};
-
-
+							});
+							return System;
+						}
+	}]);
 
 
 	function Helper(){
