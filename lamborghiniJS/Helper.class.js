9a7c98faa4f7e6015e5745f140c3abc682200c66
龙昊宏 <947923503@qq.com>
2016-07-05 09:00:36 +0800

    fix

--- lamborghiniJS/Helper.class.js
+++ lamborghiniJS/Helper.class.js
@@ -21,6 +21,39 @@ window[GRN_LHH].main(function(undefined){
 
 	System.merge(null,[{
 						/**
+						 * @author lhh
+						 * 产品介绍：
+						 * 创建日期：2015-6-25
+						 * 修改日期：2015-6-25
+						 * 名称：get_url_Param
+						 * 功能：根据指定的url参数获取相对应的参数值
+						 * 说明：
+						 * 注意：
+						 * @param   (String)name            NO NULL :参数名称
+						 * @return  String
+						 *
+						 */
+						'get_url_Param':function(name){
+							var search = document.location.search;
+							var pattern = new RegExp("[?&]"+name+"\=([^&]+)", "g");
+							var matcher = pattern.exec(search);
+							var items = null;
+							if(null != matcher){
+								try{
+									items = decodeURIComponent(decodeURIComponent(matcher[1]));
+								}catch(e){
+									try{
+										items = decodeURIComponent(matcher[1]);
+									}catch(e){
+										items = matcher[1];
+									}
+								}
+							}
+							console.log(items);
+							return items;
+
+						},
+						/**
 						 *
 						 * @author: lhh
 						 * 产品介绍：
